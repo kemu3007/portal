fe533b9345fd3b239253eda717f05d5883991ec3
kemu <kemu430113@gmail.com>
2022-03-06 19:15:51 +0900

    fix layout

--- portal/src/app/home/home.module.ts
+++ portal/src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { IndexComponent } from './index/index.component';
 import { ProfileComponent } from './profile/profile.component';
