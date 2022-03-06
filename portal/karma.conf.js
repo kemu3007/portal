29362d86cc4a4488876cec7ebd64be80fd61bfe7
kemu <kemu430113@gmail.com>
2022-03-06 17:36:19 +0900

    fix path

--- portal/karma.conf.js
+++ portal/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hp'),
+      dir: require('path').join(__dirname, './coverage/portal'),
       subdir: '.',
       reporters: [
         { type: 'html' },
