d0be48eec79f045890f0ba5a2a3221a0b0a27912
kemu <kemu430113@gmail.com>
2022-03-11 04:11:43 +0900

    fix weed

--- create_img.py
+++ create_img.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from urllib.request import urlopen
 from PIL import Image, ImageFont, ImageDraw
 
 ogp_size = (1200, 630)
@@ -7,6 +8,10 @@ font = ImageFont.truetype("./font.ttf", size=36)
 white = (255, 255, 255)
 black = (0, 0, 0)
 save_dir = Path("portal/src/assets/images")
+
+def write_weed_image():
+    weed = urlopen("https://grass-graph.appspot.com/images/kemu3007.png").read()
+    (save_dir / "weed.png").write_bytes(weed)
 
 def write_blog_image(title: str, output: str):
     image = Image.new("RGB", ogp_size, (255, 255, 255))
@@ -30,3 +35,4 @@ if __name__ == "__main__":
         write_blog_image(articles[key]["title"], key)
     write_portal_image("home", "home")
     write_portal_image("blog", "blog")
+    write_weed_image()
