4076b03d726239b1f41bc356cb4c3ed27ab429fa
kemu <kemu430113@gmail.com>
2022-03-12 15:49:48 +0900

    add sitemap

--- export.py
+++ export.py
@@ -2,6 +2,7 @@ import requests
 import json
 from pathlib import Path
 import re
+from xml.etree import ElementTree
 
 save_dir = Path("portal/src/assets/articles")
 
@@ -23,3 +24,12 @@ if __name__ == "__main__":
             f.write(json.dumps(article))
     with (save_dir / "list.json").open("x") as f:
         f.write(json.dumps(article_dict))
+    urlset = ElementTree.Element('urlset')
+    urlset.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
+    for article in articles:
+        url_element = ElementTree.SubElement(urlset, 'url')
+        loc = ElementTree.SubElement(url_element, 'loc')
+        loc.text = f"https://portal.kemu.site/blog/{article['id']}"
+        lastmod = ElementTree.SubElement(url_element, 'lastmod')
+        lastmod.text = article["created_at"][:10]
+    ElementTree.ElementTree(element=urlset).write(save_dir / 'sitemap.xml', encoding='utf-8', xml_declaration=True)
