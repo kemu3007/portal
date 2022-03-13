8f07824fa5c53a8a7f2da88db5ded9293030b304
kemu <kemu430113@gmail.com>
2022-03-13 21:44:57 +0900

    add img style

--- portal/src/app/blog/blog-detail/blog-detail.component.ts
+++ portal/src/app/blog/blog-detail/blog-detail.component.ts
@@ -9,6 +9,7 @@ import { MessageService } from '@app/message/message.service';
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
+  styleUrls: ['./blog-detail.component.scss'],
 })
 export class BlogDetailComponent implements OnInit {
   marked = marked;
