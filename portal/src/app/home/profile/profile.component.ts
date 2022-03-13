869d0453607fa4331f32e0630ec23dc92ffc0d55
kemu <kemu430113@gmail.com>
2022-03-14 00:39:34 +0900

    fix layout

--- portal/src/app/home/profile/profile.component.ts
+++ portal/src/app/home/profile/profile.component.ts
@@ -9,6 +9,6 @@ export class ProfileComponent {
   constructor(private modalService: NgbModal) {}
 
   open(content: TemplateRef<any>) {
-    this.modalService.open(content, { size: 'lg' });
+    this.modalService.open(content, { size: 'lg', centered: true });
   }
 }
