9c4718fe8c9b5ca0f3192245d99909b8aff0416f
kemu <kemu430113@gmail.com>
2022-03-14 00:24:32 +0900

    add warn

--- portal/src/app/home/home.module.ts
+++ portal/src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { IndexComponent } from './index/index.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AdsenseModule } from 'ng2-adsense';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [IndexComponent, ProfileComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    NgbModule,
     AdsenseModule.forRoot({ adClient: 'ca-pub-6219032039771696' }),
   ],
 })
