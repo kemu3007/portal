79d4c90f88f6c2986e84d0e07c914a63fd49d1ed
kemu <kemu430113@gmail.com>
2022-03-14 21:38:32 +0900

    fix pages

--- create_pages.py
+++ create_pages.py
@@ -15,3 +15,13 @@ if __name__ == "__main__":
         path = Path("portal/src/blog") / key
         path.mkdir(exist_ok=True)
         (path / "index.html").write_text(blog)
+    logs = json.loads((Path() / "portal/src/assets/logs/list.json").read_text())
+    log_home = base_html.replace("page_image", "https://portal.kemu.site/assets/images/log.png").replace("page_title", "kemu logs").replace("page_description", "kemu logs")
+    path = Path("portal/src/log")
+    path.mkdir(exist_ok=True)
+    (path / "index.html").write_text(log_home)
+    for key, data in logs.items():
+        blog = base_html.replace("page_image", data["photo"] or f"https://portal.kemu.site/assets/images/{key}.png").replace("page_title", f"kemu logs | {data['title']}").replace("page_description", data['body'])
+        path = Path("portal/src/blog") / key
+        path.mkdir(exist_ok=True)
+        (path / "index.html").write_text(blog)
