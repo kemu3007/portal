527c7a986bca744783f24f1ff658e77b9eb8698b
kemu <kemu430113@gmail.com>
2022-03-22 23:44:44 +0900

    add copy

--- portal/src/app/tools/user-info/user-info.component.ts
+++ portal/src/app/tools/user-info/user-info.component.ts
@@ -14,21 +14,35 @@ export class UserInfoComponent implements OnInit {
   data?: UserInfo;
   state = new UserInfoState();
 
-  constructor(private userInfoService: UserInfoService, private messageService: MessageService, private router: Router) {}
+  constructor(
+    private userInfoService: UserInfoService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.userInfoService.get().pipe(catchError(() => {
-      this.messageService.pushMessage({ "type": "danger", "body": "ユーザ情報の取得に失敗しました。maybe CORS 😉"})
-      this.router.navigate(["/tools"])
-      return NEVER
-    })).subscribe((data) => {
-      this.data = data;
-      this.state.pushHistory(data);
-    });
+    this.userInfoService
+      .get()
+      .pipe(
+        catchError(() => {
+          this.messageService.pushMessage({ type: 'danger', body: 'ユーザ情報の取得に失敗しました。maybe CORS 😉' });
+          this.router.navigate(['/tools']);
+          return NEVER;
+        })
+      )
+      .subscribe((data) => {
+        this.data = data;
+        this.state.pushHistory(data);
+      });
   }
 
   get adsLength(): number {
     const hasMd = window.screen.width >= 768;
     return hasMd ? 3 : 1;
+  }
+
+  copyAddress(address: string) {
+    window.navigator.clipboard.writeText(address);
+    this.messageService.pushMessage({ type: 'info', body: `${address} をクリップボードにコピーしました` });
   }
 }
