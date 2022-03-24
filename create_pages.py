8227396d12c9f336b86c30409d88c364a586774c
kemu <kemu430113@gmail.com>
2022-03-24 12:57:21 +0900

    fix html

--- create_pages.py
+++ create_pages.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     logs = json.loads((Path() / "portal/src/assets/logs/list.json").read_text())
     log_home = base_html.replace("page_image", "https://portal.kemu.site/assets/images/log.png").replace("page_title", "kemu logs").replace("page_description", "kemu logs")
     log_home_html = [f"<h2><a href='/log/{key}'>{log['title']}</a></h2><hr /> {log['body']}" for key, log in logs.items()]
-    log_home = log_home.replace("page_contents", "<hr />".join(log_home_html))    
+    log_home = log_home.replace("page_contents", "<hr />".join(log_home_html) + f"meta: {logs}")    
     path = Path("portal/src/log")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(log_home)
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     (path / "index.html").write_text(tools_html)
     # userInfo
     user_info_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/user-info.png").replace("page_title", "kemu tools | IP Address Checker").replace("page_description", "IP Address Checker")
-    user_info_html = user_info_html.replace("page_contents", "IPアドレス等の情報を確認します。 あなたのIPアドレス ...")
+    user_info_html = user_info_html.replace("page_contents", "<h2>IP Address Checker</h2>IPアドレス等の情報を確認します。 あなたのIPアドレス ..........<table><tr><th>IP Address</th><td>...</td><th>UserAgent</th><td>...</td><th>ETC...</th><td>...</td></tr>")
     path = Path("portal/src/tools/userInfo")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(user_info_html)
