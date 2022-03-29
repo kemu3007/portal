e23a167a03678064fd885e5c191a663bbf9f5fd0
kemu <kemu430113@gmail.com>
2022-03-30 00:30:33 +0900

    fix base html

--- create_pages.py
+++ create_pages.py
@@ -4,7 +4,7 @@ from pathlib import Path
 if __name__ == "__main__":
     # blog
     articles = json.loads((Path() / "portal/src/assets/articles/list.json").read_text())
-    base_html = Path('./bot.html').read_text()
+    base_html = Path('./base.html').read_text()
     blog_home = base_html.replace("page_image", "https://portal.kemu.site/assets/images/blog.png").replace("page_title", "kemu tech blog").replace("page_description", "kemu tech blog")
     blog_home_html = [f"<h2><a href='/blog/{key}'>{article['title']}</a></h2><hr /> {article['body']}" for key, article in articles.items()]
     blog_home = blog_home.replace("page_contents", "<hr />".join(blog_home_html))
@@ -42,37 +42,90 @@ if __name__ == "__main__":
     (path / "index.html").write_text(user_info_html)
     # associate link maker
     associate_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/associate.png").replace("page_title", "kemu tools | Amazon Associate Linker").replace("page_description", "アソシエイトIDからアソシエイトリンクを生成します。")
-    associate_html = associate_html.replace("page_contents", "<h2>Amazon Associate Linker</h2>Amazon Associate Linkを生成します。アソシエイトID <input /> 商品リンク <input /> <button>generate</button> result ..........")
+    associate_html = associate_html.replace("page_contents", """
+        <h2>Amazon Associate Linker</h2>
+        Amazon Associate Linkを生成します。<br />
+        アソシエイトID 
+        <input /> 
+        商品リンク <input /> 
+        <button>generate</button> 
+        result <button>copy</button>..........
+    """)
     path = Path("portal/src/tools/amazonAssociate")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(associate_html)
     # JSON Typer
     typer_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-typer.png").replace("page_title", "kemu tools | JSON Typer").replace("page_description", "JSONの値をTypeScriptのinterface型に変換します。")
-    typer_html = typer_html.replace("page_contents", "<h2>JSON Typer</h2>JSONの値をTypeScriptのinterface型に変換します。List Typeには非対応です。any[]と出力されます。<br />またJSONキーにスラッシュなどの値が含まれている場合正しく出力されないケースがあります。RootTypeName<input /> JSON <textarea></textarea> <button>TYPE</button> result ..........")
+    typer_html = typer_html.replace("page_contents", """
+        <h2>JSON Typer</h2>
+        JSONの値をTypeScriptのinterface型に変換します。List Typeには非対応です。any[]と出力されます。<br />
+        またJSONキーにスラッシュなどの値が含まれている場合正しく出力されないケースがあります。<br />
+        RootTypeName<input />
+        JSON <textarea></textarea> <button>TYPE</button> 
+        <hr />
+        result <button>copy</button> ..........
+    """)
     path = Path("portal/src/tools/jsonTyper")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(typer_html)
     # realtime
     realtime_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/realtime.png").replace("page_title", "kemu tools | REALTIME").replace("page_description", "現在時刻を100ms単位で更新し、正確な日時をお知らせします。")
-    realtime_html = realtime_html.replace("page_contents", "<h2>REALTIME</h2> 現在時刻を100ms単位で更新し、正確な日時をお知らせします。")
+    realtime_html = realtime_html.replace("page_contents", """
+        <h2>REALTIME</h2> 
+        現在時刻を100ms単位で更新し、正確な日時をお知らせします。<br />
+        <pre>
+            <h2> 2000/00/00 00:00:00 </h2>
+            <h2>000</h2>
+        </pre>
+    """)
     path = Path("portal/src/tools/realtime")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(realtime_html)
     # JSON Formatter
     formatter_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-formatter.png").replace("page_title", "kemu tools | JSON Formatter").replace("page_description", "未整形のJSONデータを整形します。")
-    formatter_html = formatter_html.replace("page_contents", "<h2>JSON Formatter</h2> 未整形のJSONデータを整形します。 <input /> <textarea></textarea>")
+    formatter_html = formatter_html.replace("page_contents", """
+        <h2>JSON Formatter</h2> 
+        未整形のJSONデータを整形します。 <br />
+        <input /> 
+        <button>format</button>
+        <hr />
+        result <button>copy</button>
+        ...........................
+    """)
     path = Path("portal/src/tools/jsonFormatter")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(formatter_html)
     # Base64 Encoder/Decoder
     base64_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/base64.png").replace("page_title", "kemu tools | Base64 Encoder/Decoder").replace("page_description", "文字列データをBase64に、Base64データを文字列に変換します。")
-    base64_html = base64_html.replace("page_contents", "<h2>Base64 Encoder/Decoder</h2> 文字列データをBase64に、Base64データを文字列に変換します。 <input /> <textarea></textarea>")
+    base64_html = base64_html.replace("page_contents", """
+        <h2>Base64 Encoder/Decoder</h2> 
+        文字列データをBase64に、Base64データを文字列に変換します。 
+        base64 <br />
+        <textarea></textarea>
+        string <br />
+        <textarea></textarea>
+        <hr />
+        result <button>copy</button>
+        .............................
+
+        string <br />
+        <textarea></textarea>
+        base64 <br />
+        <textarea></textarea>
+    """)
     path = Path("portal/src/tools/base64")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(base64_html)
     # contact
     contact_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/contact.png").replace("page_title", "Contact").replace("page_description", "Contact")
-    contact_html = contact_html.replace("page_contents", "仕事の依頼などは以下のメールアドレスにお願いします。セールスメールはお控えください。")
+    contact_html = contact_html.replace("page_contents", """
+        <h2> Contact Me </h2>
+        仕事の依頼などは以下のメールアドレスにお願いします。セールスメールはお控えください。<br />
+        TwitterのDMも可能ですが返信が遅くなってしまう可能性があるためできる限りメールでお願いいたします。<br />
+        セールスメールを送信した場合メールアドレスをブロックするため以降のメールは全て受信拒否を行います。ご了承ください。<br />
+        <hr />
+        *********@*****.***
+    """)
     path = Path("portal/src/contact")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(contact_html)
