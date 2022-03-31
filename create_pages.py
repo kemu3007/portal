31dbe9a0b9cc06d7187ab9f3a77c9665e4f31e8f
kemu <kemu430113@gmail.com>
2022-04-01 00:02:21 +0900

    fix page title

--- create_pages.py
+++ create_pages.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(blog_home)
     for key, data in articles.items():
-        blog = base_html.replace("page_image", f"https://portal.kemu.site/assets/images/{key}.png").replace("page_title", f"kemu tech blog | {data['title']}").replace("page_description", data['body'])
+        blog = base_html.replace("page_image", f"https://portal.kemu.site/assets/images/{key}.png").replace("page_title", f"{data['title']} | kemu tech blog").replace("page_description", data['body'])
         blog_details = json.loads((Path() / f"portal/src/assets/articles/{key}.json").read_text())
         blog = blog.replace("page_contents", f"<h2>{data['title']}</h2> {blog_details['body']} <hr /> meta: {blog_details}")
         path = Path("portal/src/blog") / key
@@ -35,13 +35,29 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(tools_html)
     # ip address checker
-    user_info_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/user-info.png").replace("page_title", "kemu tools | IP Address Checker").replace("page_description", "IP Addressなどの端末情報を確認できます。")
-    user_info_html = user_info_html.replace("page_contents", "<h2>IP Address Checker</h2>IPアドレス等の情報を確認します。 あなたのIPアドレス ..........<table><tr><th>IP Address</th><td>...</td><th>UserAgent</th><td>...</td><th>ETC...</th><td>...</td></tr>")
+    user_info_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/user-info.png").replace("page_title", "IP Address Checker | kemu tools").replace("page_description", "IP Addressなどの端末情報を確認できます。")
+    user_info_html = user_info_html.replace("page_contents", """
+        <h2>IP Address Checker</h2>
+        IPアドレス等の情報を確認します。 あなたのIPアドレス ..........
+        <table>
+            <tr>
+                <th>IP Address</th>
+                <td>0.0.0.0</td>
+            </tr>
+            <tr>
+                <th>UserAgent</th>
+                <td>...</td>
+            </tr>
+            <tr>
+                <th>ETC...</th><td>...</td>
+            </tr>
+        </table>
+        """)
     path = Path("portal/src/tools/userInfo")
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(user_info_html)
     # associate link maker
-    associate_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/associate.png").replace("page_title", "kemu tools | Amazon Associate Linker").replace("page_description", "アソシエイトIDからアソシエイトリンクを生成します。")
+    associate_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/associate.png").replace("page_title", "Amazon Associate Linker | kemu tools").replace("page_description", "アソシエイトIDからアソシエイトリンクを生成します。")
     associate_html = associate_html.replace("page_contents", """
         <h2>Amazon Associate Linker</h2>
         Amazon Associate Linkを生成します。<br />
@@ -55,7 +71,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(associate_html)
     # JSON Typer
-    typer_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-typer.png").replace("page_title", "kemu tools | JSON Typer").replace("page_description", "JSONの値をTypeScriptのinterface型に変換します。")
+    typer_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-typer.png").replace("page_title", "JSON Typer | kemu tools").replace("page_description", "JSONの値をTypeScriptのinterface型に変換します。")
     typer_html = typer_html.replace("page_contents", """
         <h2>JSON Typer</h2>
         JSONの値をTypeScriptのinterface型に変換します。List Typeには非対応です。any[]と出力されます。<br />
@@ -69,7 +85,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(typer_html)
     # realtime
-    realtime_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/realtime.png").replace("page_title", "kemu tools | REALTIME").replace("page_description", "現在時刻を100ms単位で更新し、正確な日時をお知らせします。")
+    realtime_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/realtime.png").replace("page_title", "REALTIME | kemu tools").replace("page_description", "現在時刻を100ms単位で更新し、正確な日時をお知らせします。")
     realtime_html = realtime_html.replace("page_contents", """
         <h2>REALTIME</h2> 
         現在時刻を100ms単位で更新し、正確な日時をお知らせします。<br />
@@ -82,7 +98,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(realtime_html)
     # JSON Formatter
-    formatter_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-formatter.png").replace("page_title", "kemu tools | JSON Formatter").replace("page_description", "未整形のJSONデータを整形します。")
+    formatter_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/json-formatter.png").replace("page_title", "JSON Formatter | kemu tools").replace("page_description", "未整形のJSONデータを整形します。")
     formatter_html = formatter_html.replace("page_contents", """
         <h2>JSON Formatter</h2> 
         未整形のJSONデータを整形します。 <br />
@@ -96,7 +112,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(formatter_html)
     # Base64 Encoder/Decoder
-    base64_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/base64.png").replace("page_title", "kemu tools | Base64 Encoder/Decoder").replace("page_description", "文字列データをBase64に、Base64データを文字列に変換します。")
+    base64_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/base64.png").replace("page_title", "Base64 Encoder/Decoder | kemu tools").replace("page_description", "文字列データをBase64に、Base64データを文字列に変換します。")
     base64_html = base64_html.replace("page_contents", """
         <h2>Base64 Encoder/Decoder</h2> 
         文字列データをBase64に、Base64データを文字列に変換します。 
@@ -117,7 +133,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(base64_html)
     # contact
-    contact_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/contact.png").replace("page_title", "Contact").replace("page_description", "Contact")
+    contact_html = base_html.replace("page_image", "https://portal.kemu.site/assets/images/contact.png").replace("page_title", "Contact | kemu portal").replace("page_description", "Contact")
     contact_html = contact_html.replace("page_contents", """
         <h2> Contact Me </h2>
         仕事の依頼などは以下のメールアドレスにお願いします。セールスメールはお控えください。<br />
@@ -130,7 +146,7 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True)
     (path / "index.html").write_text(contact_html)
     for key, data in logs.items():
-        log = base_html.replace("page_image", data["photo"] or f"https://portal.kemu.site/assets/images/{key}.png").replace("page_title", f"kemu logs | {data['title']}").replace("page_description", data['body'])
+        log = base_html.replace("page_image", data["photo"] or f"https://portal.kemu.site/assets/images/{key}.png").replace("page_title", f"{data['title']} | kemu logs").replace("page_description", data['body'])
         log_details = json.loads((Path() / f"portal/src/assets/logs/{key}.json").read_text())
         log = log.replace("page_contents", f"<h2>{data['title']}</h2><hr />{log_details['body']}<hr /> meta: {log_details}")
         path = Path("portal/src/log") / key
