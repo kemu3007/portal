0eeb9a3266235af4fabba78ed5313436ac6b207e
kemu <kemu430113@gmail.com>
2022-04-25 18:25:13 +0900

    add rss

---
+++ create_rss.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+from typing import Any, Dict
+from xml.etree import ElementTree
+
+articles_list: Dict[str, Any] = json.loads(Path("portal/src/assets/articles/list.json").read_text())
+
+
+def append_child(parent: ElementTree.Element, tag: str, text: str):
+    child = ElementTree.SubElement(parent, tag)
+    child.text = text
+
+
+if __name__ == "__main__":
+    rss = ElementTree.Element("rss")
+    rss.set("version", "2.0")
+    channel = ElementTree.SubElement(rss, "channel")
+
+    append_child(channel, "title", "Kemu Tech Blog")
+    append_child(channel, "link", "https://portal.kemu.site/blog/")
+    append_child(channel, "description", "Kemu Tech Blogでは主にDjango / Angularに関する技術的な知見のメモ、共有を行っています。")
+    append_child(channel, "language", "ja")
+    append_child(channel, "copyright", "©️ 2017-2022 kemu All Rights Reserved.")
+    append_child(channel, "managingEditor", "kemu430113@gmail.com(kemu)")
+    append_child(channel, "webMaster", "kemu430113@gmail.com(kemu)")
+
+    for id in list(reversed(articles_list.keys()))[:15]:
+        item = ElementTree.SubElement(channel, "item")
+        article = articles_list[id]
+
+        append_child(item, "title", article["title"])
+        append_child(item, "link", f"https://portal.kemu.site/blog/{id}/")
+        append_child(item, "description", article["body"])
+        append_child(item, "pubDate", article["created_at"])
+
+    ElementTree.ElementTree(element=rss).write("portal/src/rss.xml", encoding="utf-8", xml_declaration=True)
--- create_sitemap.py
+++ create_sitemap.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from pathlib import Path
 from xml.etree import ElementTree
 
@@ -8,41 +9,41 @@ logs_list = json.loads(Path("portal/src/assets/logs/list.json").read_text())
 urlset = ElementTree.Element("urlset")
 
 
-def add_url_element(url):
-    url_element = ElementTree.SubElement(urlset, "url")
+def add_url_element(parent: ElementTree.Element, url: str):
+    url_element = ElementTree.SubElement(parent, "url")
     loc = ElementTree.SubElement(url_element, "loc")
     loc.text = url
     lastmod = ElementTree.SubElement(url_element, "lastmod")
-    lastmod.text = "2022-04-02"
+    lastmod.text = str(datetime.now().date())
 
 
 if __name__ == "__main__":
     urlset = ElementTree.Element("urlset")
     urlset.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
 
-    add_url_element("https://portal.kemu.site/")
-    add_url_element("https://portal.kemu.site/robots.txt")
-    add_url_element("https://portal.kemu.site/sitemap.xml")
-    add_url_element("https://portal.kemu.site/blog/")
-    add_url_element("https://portal.kemu.site/log/")
-    add_url_element("https://portal.kemu.site/contact/")
+    add_url_element(urlset, "https://portal.kemu.site/")
+    add_url_element(urlset, "https://portal.kemu.site/robots.txt")
+    add_url_element(urlset, "https://portal.kemu.site/sitemap.xml")
+    add_url_element(urlset, "https://portal.kemu.site/blog/")
+    add_url_element(urlset, "https://portal.kemu.site/log/")
+    add_url_element(urlset, "https://portal.kemu.site/contact/")
 
-    add_url_element("https://portal.kemu.site/tools/")
-    add_url_element("https://portal.kemu.site/tools/userInfo/")
-    add_url_element("https://portal.kemu.site/tools/amazonAssociate/")
-    add_url_element("https://portal.kemu.site/tools/jsonTyper/")
-    add_url_element("https://portal.kemu.site/tools/realtime/")
-    add_url_element("https://portal.kemu.site/tools/jsonFormatter/")
-    add_url_element("https://portal.kemu.site/tools/base64/")
-    add_url_element("https://portal.kemu.site/tools/mdwriter/")
-    add_url_element("https://portal.kemu.site/tools/regex/")
-    add_url_element("https://portal.kemu.site/tools/year/")
-    add_url_element("https://portal.kemu.site/tools/openapi/")
-    add_url_element("https://portal.kemu.site/doc/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/userInfo/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/amazonAssociate/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/jsonTyper/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/realtime/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/jsonFormatter/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/base64/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/mdwriter/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/regex/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/year/")
+    add_url_element(urlset, "https://portal.kemu.site/tools/openapi/")
+    add_url_element(urlset, "https://portal.kemu.site/doc/")
 
     for article_id in articles_list.keys():
-        add_url_element(f"https://portal.kemu.site/blog/{article_id}/")
+        add_url_element(urlset, f"https://portal.kemu.site/blog/{article_id}/")
     for log_id in logs_list.keys():
-        add_url_element(f"https://portal.kemu.site/log/{log_id}/")
+        add_url_element(urlset, f"https://portal.kemu.site/log/{log_id}/")
 
     ElementTree.ElementTree(element=urlset).write("portal/src/sitemap.xml", encoding="utf-8", xml_declaration=True)
