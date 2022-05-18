ceae6368c0b3e394416505364c0dfcd576eb7ec5
kemu <kemu430113@gmail.com>
2022-05-18 23:47:42 +0900

    fix tweet btn

--- portal/src/app/shared/tweet-btn/tweet-btn.component.ts
+++ portal/src/app/shared/tweet-btn/tweet-btn.component.ts
@@ -1,23 +1,37 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tweet-btn',
-  template: ` <div style="height: 30px;">
-    <a id="btn" href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" [attr.data-url]="url"
-      >tweet</a
-    >
-  </div>`,
+  template: `
+    <button class="btn btn-light border rounded-pill" (mouseover)="mouseOver()" (mouseout)="mouseOut()">
+      <a id="btn" [href]="href" target="_blank"><i class="bi bi-twitter"></i>{{ btnText }}</a>
+    </button>
+  `,
 })
-export class TweetBtnComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://platform.twitter.com/widgets.js';
-    const btn = document.getElementById('btn')!;
-    btn.insertAdjacentElement('afterend', script);
+export class TweetBtnComponent {
+  btnText = ""
+
+  get href() {
+    const href = new URL('https://twitter.com/share');
+    href.searchParams.set('url', this.url);
+    href.searchParams.set('text', this.text);
+    href.searchParams.set('via', 'kemu43');
+    return href;
+  }
+
+  mouseOver() {
+    this.btnText = "Share This Page"
+  }
+
+  mouseOut() {
+    this.btnText = ""
   }
 
   get url() {
     return window.location.origin + '/$' + window.location.pathname + window.location.search;
+  }
+
+  get text() {
+    return document.title;
   }
 }
