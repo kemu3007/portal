3f713e74e0009a63012c0dead19482265fecf0f2
kemu <kemu430113@gmail.com>
2022-05-29 00:05:19 +0900

    useSentry

--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { APP_INITIALIZER, CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import * as Sentry from '@sentry/angular';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, SharedModule, BrowserAnimationsModule, ScullyLibModule],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'ja' }],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ja' },
+    { provide: ErrorHandler, useValue: Sentry.createErrorHandler({ showDialog: false }) },
+    { provide: Sentry.TraceService, deps: [Router] },
+    { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- portal/src/main.ts
+++ portal/src/main.ts
@@ -2,6 +2,19 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: environment.sentryDSN,
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ['localhost', 'https://portal.kemu.site'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
