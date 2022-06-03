7b38ac7d163704466c4d0b67f7f5e1d844dc3106
kemu <kemu430113@gmail.com>
2022-06-03 20:42:00 +0900

    fix link

--- create_meta.py
+++ create_meta.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     logs: Dict[str, Article] = json.loads((Path() / "portal/src/assets/logs/list.json").read_text())
 
     create_meta(
-        "/assets/images/blog.png",
+        "https://portal.kemu.site/assets/images/blog.png",
         "Kemu Tech Blog",
         """
         Angular / DjangoがメインのTech Noteです。
@@ -37,13 +37,13 @@ if __name__ == "__main__":
     )
     for key, data in articles.items():
         create_meta(
-            f"/assets/images/en/{key}.png",
+            f"https://portal.kemu.site/assets/images/en/{key}.png",
             f"{data['title']} | Kemu Tech Blog",
             data["body"],
             f"/blog/{key}",
         )
     create_meta(
-        "/assets/images/blog.png",
+        "https://portal.kemu.site/assets/images/blog.png",
         "Kemu Tech Blog ~English~",
         """
         Kemu's Tech Note about Angular / Django / etc...
@@ -52,13 +52,13 @@ if __name__ == "__main__":
     )
     for key, data in en_articles.items():
         create_meta(
-            f"/assets/images/en/{key}.png",
+            f"https://portal.kemu.site/assets/images/en/{key}.png",
             f"{data['title']} | Kemu Tech Blog",
             data["body"],
             f"/blog/en/{key}",
         )
     create_meta(
-        "/assets/images/zh/blog.png",
+        "https://portal.kemu.site/assets/images/zh/blog.png",
         "Kemu Tech Blog ~Chinese~",
         """
         Kemu's Tech Note about Angular / Django / etc...
@@ -67,14 +67,14 @@ if __name__ == "__main__":
     )
     for key, data in zh_articles.items():
         create_meta(
-            f"/assets/images/en/{key}.png",
+            f"https://portal.kemu.site/assets/images/en/{key}.png",
             f"{data['title']} | Kemu Tech Blog",
             data["body"],
             f"/blog/zh/{key}",
         )
     # log
     create_meta(
-        "/assets/images/log.png",
+        "https://portal.kemu.site/assets/images/log.png",
         "kemu 旅行記録など雑記 | Kemu Log",
         """
         旅行ログなど雑記置き場、日ごろは東京にいたり淡路島にいたり大阪にいたり。
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     )
     for key, data in logs.items():
         create_meta(
-            f"/assets/images/{key}.png",
+            f"https://portal.kemu.site/assets/images/{key}.png",
             f"{data['title']} | Kemu Log",
             data["body"],
             f"/log/{key}",
@@ -92,14 +92,14 @@ if __name__ == "__main__":
 
     # tools
     create_meta(
-        "/assets/images/tools.png",
+        "https://portal.kemu.site/assets/images/tools.png",
         "Kemu Tools",
         "自作ツール置き場 / Markdown Writer, QrCode Maker, JSON Typer, JSON Formatter, Base64 Translator...",
         "/tools",
     )
     # ip address checker
     create_meta(
-        "/assets/images/user-info.png",
+        "https://portal.kemu.site/assets/images/user-info.png",
         "IP Address Checker | Kemu Tools",
         """
         IP Address, User Agentなどの端末情報を確認することができ、結果を自動的にローカルストレージに保存することで、過去データとの称号を可能にしています。
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     )
     # associate link maker
     create_meta(
-        "/assets/images/associate.png",
+        "https://portal.kemu.site/assets/images/associate.png",
         "Amazon Associate Linker | Kemu Tools",
         """
         Amazon Associate IDと商品URLを入力することで商品リンクにアソシエイトIDの紐付けを可能にしています。
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     )
     # JSON Typer
     create_meta(
-        "/assets/images/json-typer.png",
+        "https://portal.kemu.site/assets/images/json-typer.png",
         "JSON Typer | Kemu Tools",
         """
         任意のJSONの値をTypeScriptのinterface型に変換します。
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     # realtime
 
     create_meta(
-        "/assets/images/realtime.png",
+        "https://portal.kemu.site/assets/images/realtime.png",
         "REALTIME | Kemu Tools",
         """
         現在時刻を100ms単位で更新し、正確な日時をお知らせします。
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     # JSON Formatter
 
     create_meta(
-        "/assets/images/json-formatter.png",
+        "https://portal.kemu.site/assets/images/json-formatter.png",
         "JSON Formatter | Kemu Tools",
         """
         未整形のJSONデータを整形します。
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     # Base64 Encoder/Decoder
 
     create_meta(
-        "/assets/images/base64.png",
+        "https://portal.kemu.site/assets/images/base64.png",
         "Base64 Encoder/Decoder | Kemu Tools",
         """
         文字列データをBase64に、Base64データを文字列に変換します。
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     # GitHub Flavored Markdown Writer
 
     create_meta(
-        "/assets/images/mdwriter.png",
+        "https://portal.kemu.site/assets/images/mdwriter.png",
         "GitHub Flavored Markdown Writer | Kemu Tools",
         """
         GitHub Flavored MarkdownをHTMLに変換します。変換データ及び元のMarkdwonデータをワンクリックでコピー可能です。
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     # Regex Checker
 
     create_meta(
-        "/assets/images/regex.png",
+        "https://portal.kemu.site/assets/images/regex.png",
         "Regex Checker | Kemu Tools",
         """
         正規表現とテキストを入力することでテキストを対象とした正規表現のテストを行い、index, マッチ文字列を返却します。
@@ -188,7 +188,7 @@ if __name__ == "__main__":
     # Year Checker
 
     create_meta(
-        "/assets/images/year.png",
+        "https://portal.kemu.site/assets/images/year.png",
         "Year Checker | Kemu Tools",
         """
         誕生日をもとに各種卒業年度/厄年といった年月日を計算します。
@@ -199,7 +199,7 @@ if __name__ == "__main__":
     # OpenApi Viewer
 
     create_meta(
-        "/assets/images/openapi.png",
+        "https://portal.kemu.site/assets/images/openapi.png",
         "OpenApi Viewer | Kemu Tools",
         """
         OpenApi JSON/YAMLをSwagger-UI / Redoc UIで表示します。
@@ -210,7 +210,7 @@ if __name__ == "__main__":
     # Mermaid Live Editor
 
     create_meta(
-        "/assets/images/mermaid.png",
+        "https://portal.kemu.site/assets/images/mermaid.png",
         "Mermaid Live Editor | Kemu Tools",
         """
         Mermaidをリアルタイムで描画し、記述を支援します。
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     # contact
 
     create_meta(
-        "/assets/images/contact.png",
+        "https://portal.kemu.site/assets/images/contact.png",
         "Contact | Kemu Portal",
         """
         仕事の依頼/転職の誘いなどはこちらからお願いします。セールスメールはお控えください。
