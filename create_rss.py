9f8c29c4ae736852ea048bff193352f4a8336a8b
kemu <kemu430113@gmail.com>
2022-06-03 21:57:53 +0900

    add rss

--- create_rss.py
+++ create_rss.py
@@ -12,15 +12,15 @@ Article = TypedDict(
 )
 
 articles: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/list.json").read_text())
+
+articles_en: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/en/list.json").read_text())
+
+articles_zh: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/zh/list.json").read_text())
+
 logs: Dict[str, Article] = json.loads(Path("portal/src/assets/logs/list.json").read_text())
 
 
-def append_child(parent: ElementTree.Element, tag: str, text: str):
-    child = ElementTree.SubElement(parent, tag)
-    child.text = text
-
-
-if __name__ == "__main__":
+def create_ja_rss() -> ElementTree.Element:
     rss = ElementTree.Element("rss")
     rss.set("version", "2.0")
     rss.set("xmlns:atom", "http://www.w3.org/2005/Atom")
@@ -62,12 +62,66 @@ if __name__ == "__main__":
 
         append_child(item, "description", escape(article["body"]))
 
-        append_child(item, "category", ", ".join([label["name"] for label in article["labels"]]))
-
         append_child(
             item,
             "pubDate",
             datetime.strptime(article["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%a, %d %b %Y %H:%M:%S +0900"),
         )
+    return rss
 
-    ElementTree.ElementTree(element=rss).write("portal/src/rss.xml", encoding="utf-8", xml_declaration=True)
+
+def create_another_lang_rss(lang: str, articles: Dict[str, Article]) -> ElementTree.Element:
+    rss = ElementTree.Element("rss")
+    rss.set("version", "2.0")
+    rss.set("xmlns:atom", "http://www.w3.org/2005/Atom")
+    channel = ElementTree.SubElement(rss, "channel")
+    atom_link = ElementTree.SubElement(channel, "atom:link")
+    atom_link.set("href", f"https://portal.kemu.site/rss_{lang}.xml")
+    atom_link.set("rel", "self")
+    atom_link.set("type", "application/rss+xml")
+
+    append_child(channel, "lastBuildDate", datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0900"))
+    append_child(channel, "category", "Python/TypeScript/Django/Angular/Nuxt/Vue/Docker/GitHub Pages")
+    append_child(channel, "title", "Kemu Tech Blog")
+    append_child(channel, "link", "https://portal.kemu.site/")
+    append_child(channel, "description", "Kemu's Tech Note about Angular / Django / etc...")
+    append_child(channel, "language", lang)
+    append_child(channel, "copyright", "©️ 2017-2022 kemu All Rights Reserved.")
+    append_child(channel, "managingEditor", "kemu430113@gmail.com(kemu)")
+    append_child(channel, "webMaster", "kemu430113@gmail.com(kemu)")
+    append_child(channel, "generator", "https://github.com/kemu3007/portal/blob/master/create_rss.py")
+
+    for id in sorted(articles.keys(), reverse=True):
+        item = ElementTree.SubElement(channel, "item")
+
+        guid = ElementTree.SubElement(item, "guid")
+        guid.set("isPermaLink", "false")
+        guid.text = id
+
+        article = articles[id]
+
+        append_child(item, "title", escape(article["title"]))
+        append_child(item, "link", f"https://portal.kemu.site/blog/{lang}/{id}/")
+        append_child(item, "category", "/".join([label["name"] for label in article["labels"]]))
+        append_child(item, "description", escape(article["body"]))
+        append_child(
+            item,
+            "pubDate",
+            datetime.strptime(article["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%a, %d %b %Y %H:%M:%S +0900"),
+        )
+    return rss
+
+
+def append_child(parent: ElementTree.Element, tag: str, text: str):
+    child = ElementTree.SubElement(parent, tag)
+    child.text = text
+
+
+if __name__ == "__main__":
+    ElementTree.ElementTree(element=create_ja_rss()).write("portal/src/rss.xml", encoding="utf-8", xml_declaration=True)
+    ElementTree.ElementTree(element=create_another_lang_rss("en", articles_en)).write(
+        "portal/src/rss_en.xml", encoding="utf-8", xml_declaration=True
+    )
+    ElementTree.ElementTree(element=create_another_lang_rss("zh", articles_zh)).write(
+        "portal/src/rss_zh.xml", encoding="utf-8", xml_declaration=True
+    )
