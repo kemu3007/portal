4fe6b2e12a64644bbdab2744cf9b08bff882bffd
kemu <kemu430113@gmail.com>
2022-06-10 21:52:12 +0900

    add tools rss

--- create_rss.py
+++ create_rss.py
@@ -11,6 +11,8 @@ Article = TypedDict(
     "Article", {"title": str, "body": str, "created_at": str, "photo": Optional[str], "labels": List[Label]}
 )
 
+Tool = TypedDict("Tool", {"label": str, "link": str, "description": bool, "external": bool, "pinned": bool})
+
 articles: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/list.json").read_text())
 
 articles_en: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/en/list.json").read_text())
@@ -18,6 +20,8 @@ articles_en: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/en
 articles_zh: Dict[str, Article] = json.loads(Path("portal/src/assets/articles/zh/list.json").read_text())
 
 logs: Dict[str, Article] = json.loads(Path("portal/src/assets/logs/list.json").read_text())
+
+tools: List[Tool] = json.loads(Path("portal/src/assets/tools.json").read_text())
 
 
 def create_ja_rss() -> ElementTree.Element:
@@ -112,6 +116,42 @@ def create_another_lang_rss(lang: str, articles: Dict[str, Article]) -> ElementT
     return rss
 
 
+def create_tools_rss() -> ElementTree.Element:
+    rss = ElementTree.Element("rss")
+    rss.set("version", "2.0")
+    rss.set("xmlns:atom", "http://www.w3.org/2005/Atom")
+    channel = ElementTree.SubElement(rss, "channel")
+    atom_link = ElementTree.SubElement(channel, "atom:link")
+    atom_link.set("href", f"https://portal.kemu.site/tools_rss.xml")
+    atom_link.set("rel", "self")
+    atom_link.set("type", "application/rss+xml")
+
+    append_child(channel, "lastBuildDate", datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0900"))
+    append_child(channel, "category", "Python/TypeScript/Django/Angular/Nuxt/Vue/Docker/GitHub Pages")
+    append_child(channel, "title", "Kemu Tools")
+    append_child(channel, "link", "https://portal.kemu.site/")
+    append_child(channel, "description", "Mermaid Live Editor, GitHub Flavored Markdown, Regex Checker, JSON Typer...")
+    append_child(channel, "language", "ja")
+    append_child(channel, "copyright", "©️ 2017-2022 kemu All Rights Reserved.")
+    append_child(channel, "managingEditor", "kemu430113@gmail.com(kemu)")
+    append_child(channel, "webMaster", "kemu430113@gmail.com(kemu)")
+    append_child(channel, "generator", "https://github.com/kemu3007/portal/blob/master/create_rss.py")
+
+    for tool in filter(lambda x: not x["external"], tools):
+        item = ElementTree.SubElement(channel, "item")
+
+        guid = ElementTree.SubElement(item, "guid")
+        guid.set("isPermaLink", "true")
+        guid.text = tool["link"]
+
+        append_child(item, "title", tool["label"])
+        append_child(item, "link", f"https://portal.kemu.site{tool['link']}")
+        append_child(item, "category", "Tech Tool")
+        append_child(item, "description", tool["description"])
+        append_child(item, "pubDate", datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0900"))
+    return rss
+
+
 def append_child(parent: ElementTree.Element, tag: str, text: str):
     child = ElementTree.SubElement(parent, tag)
     child.text = text
@@ -124,4 +164,7 @@ if __name__ == "__main__":
     )
     ElementTree.ElementTree(element=create_another_lang_rss("zh", articles_zh)).write(
         "portal/src/rss_zh.xml", encoding="utf-8", xml_declaration=True
+    )
+    ElementTree.ElementTree(element=create_tools_rss()).write(
+        "portal/src/tools_rss.xml", encoding="utf-8", xml_declaration=True
     )
