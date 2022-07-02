e306d31276007ef271a127002d8ee4c132bd7203
kemu <kemu430113@gmail.com>
2022-07-03 00:21:19 +0900

    fix nits

--- portal/scully/plugins/plugin.ts
+++ portal/scully/plugins/plugin.ts
@@ -2,6 +2,8 @@
 import { registerPlugin, HandledRoute } from '@scullyio/scully';
 import logs from '../../src/assets/logs/list.json';
 import blogs from '../../src/assets/articles/list.json';
+import enBlogs from '../../src/assets/articles/en/list.json'
+import zhBlogs from '../../src/assets/articles/zh/list.json'
 
 
 export const myPlugin = 'myPlugin';
@@ -30,7 +32,7 @@ function blogRoutesPlugin(_route: string, _config = {}) {
 
 registerPlugin('router', "blogIds", blogRoutesPlugin, [])
 
-const blogEnRoutes: HandledRoute[] = Object.keys(blogs).map(blogId => ({ route: `/blog/en/${blogId}` }))
+const blogEnRoutes: HandledRoute[] = Object.keys(enBlogs).map(blogId => ({ route: `/blog/en/${blogId}` }))
 
 function blogEnRoutesPlugin(_route: string, _config = {}) {
   return Promise.resolve(blogEnRoutes)
@@ -38,7 +40,7 @@ function blogEnRoutesPlugin(_route: string, _config = {}) {
 
 registerPlugin('router', "blogEnIds", blogEnRoutesPlugin, [])
 
-const blogZhRoutes: HandledRoute[] = Object.keys(blogs).map(blogId => ({ route: `/blog/zh/${blogId}` }))
+const blogZhRoutes: HandledRoute[] = Object.keys(zhBlogs).map(blogId => ({ route: `/blog/zh/${blogId}` }))
 
 function blogZhRoutesPlugin(_route: string, _config = {}) {
   return Promise.resolve(blogZhRoutes)
