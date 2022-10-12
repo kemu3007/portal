4e1cf9598673304bbde0032705fa87a536f3c59a
kemu <kemu430113@gmail.com>
2022-10-12 23:01:00 +0900

    add interval

--- portal/src/app/shared/loading/loading.service.ts
+++ portal/src/app/shared/loading/loading.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
-  loading = false;
+  _loading = false;
+
+  set loading(value: boolean) {
+    if (!value) {
+      interval(500)
+        .pipe(take(1))
+        .subscribe((_) => (this._loading = value));
+    } else {
+      this._loading = value;
+    }
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
 }
