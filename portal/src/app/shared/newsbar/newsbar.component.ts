9863579d04424713470b0fd5f5e05b297030b523
kemu <kemu430113@gmail.com>
2023-07-21 03:40:32 +0900

    fix link

--- portal/src/app/shared/newsbar/newsbar.component.ts
+++ portal/src/app/shared/newsbar/newsbar.component.ts
@@ -19,14 +19,6 @@ export class NewsbarComponent implements OnInit {
     interval(5000).subscribe((_) => (this.index += 1));
   }
 
-  get latestArticleId() {
-    return Object.keys(this.articles).sort((a, b) => (a > b ? -1 : 1))[0] ?? '';
-  }
-
-  get latestArticleTitle() {
-    return this.articles[this.latestArticleId]?.title ?? '';
-  }
-
   get news() {
     return this.newsList[this.index % this.newsList.length];
   }
@@ -34,21 +26,12 @@ export class NewsbarComponent implements OnInit {
   get newsList() {
     return [
       {
-        type: 'info',
-        contents:
-          "Google Newsで配信を受け取ることが可能です。<a href='https://news.google.com/publications/CAAqBwgKMJmRuAswpqzPAw?ceid=JP:ja&oc=3' target='_blank'>こちら</a>から",
-      },
-      {
         type: 'warning',
         contents: 'このサイトは広告収益で成り立っています。広告ブロックツールは停止してください。',
       },
       {
         type: 'info',
         contents: '本ブログは組織としての見解ではなく、個人の見解を示すものとなっております。ご承知の上閲覧ください。',
-      },
-      {
-        type: 'info',
-        contents: `新着記事通知: <a href='./blog/${this.latestArticleId}/'>${this.latestArticleTitle}</a>`,
       },
     ];
   }
