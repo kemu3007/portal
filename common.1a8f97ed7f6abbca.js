2eb6905c82651f199f113a5f26b438485121d3e1
kemu3007 <kemu3007@users.noreply.github.com>
2023-09-11 10:40:43 +0000

    deploy: 16a60ed44a28f174c7e5c380a9a917fc25979335

---
+++ common.1a8f97ed7f6abbca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkportal=self.webpackChunkportal||[]).push([[592],{939:(l,s,o)=>{o.d(s,{G:()=>a});var r=o(5e3),n=o(520);let a=(()=>{class t{constructor(e){this.httpClient=e}get(){return this.httpClient.get("assets/tools.json")}}return t.\u0275fac=function(e){return new(e||t)(r.LFG(n.eN))},t.\u0275prov=r.Yz7({token:t,factory:t.\u0275fac,providedIn:"root"}),t})()}}]);
